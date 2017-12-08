b77c8ef7af8900a40a6321603f2340abf0daa948
Roberth Riera <riera_1992@hotmail.com>
2017-12-07 21:15:18 -0400

    first commit

---
+++ Angular.php
@@ -0,0 +1,12 @@
+
+1. Instalar proyecto 
+
+Para crear el proyecto se debe instalar en primer lugar nodeJs, luego se procede a correr los siguientes comandos:
+
+npm install -g @angular/cli
+
+ng new angular4
+
+cd angular4
+
+ng serve
